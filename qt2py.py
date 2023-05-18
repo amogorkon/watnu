95361d8d928d2badf48db1514a549a40cab45841
Anselm Kiefner <watnu@anselm.kiefner.de>
2023-05-18 18:30:42 +0200

    ruff

--- qt2py.py
+++ qt2py.py
@@ -5,22 +5,32 @@ ui_path = Path("./src/ui")
 
 
 for path in ui_path.glob("*.ui"):
-    args = [r"C:\Users\micro\AppData\Local\Programs\Python\Python311\Scripts\pyuic6.exe", path, "-o", f"src/ui/{path.stem}.py"]
+    args = [
+        r"C:\Users\micro\AppData\Local\Programs\Python\Python311\Scripts\pyuic6.exe",
+        path,
+        "-o",
+        f"src/ui/{path.stem}.py",
+    ]
     print(args)
     rc = run(args)
     (Path("./src/ux") / path.name).with_suffix(".py").touch()
 
 
-Path("./src/ui/__init__.py").write_text(f"""
+Path("./src/ui/__init__.py").write_text(
+    f"""
 # generated via qt2py.py
 from . import (
 {', '.join(p.stem for p in ui_path.glob("*.py"))}
 )
-""")
+"""
+)
 
-Path("./src/ux/__init__.py").write_text(f"""
+Path("./src/ux/__init__.py").write_text(
+    f"""
+# ruff: noqa: F401
 # generated via qt2py.py
 from . import (
 {', '.join(p.stem for p in ui_path.glob("*.py"))}
 )
-""")
+"""
+)
--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -1,5 +1,5 @@
 
 # generated via qt2py.py
 from . import (
-about, attributions, character, choose_constraints, choose_deadline, choose_repeats, choose_skills, choose_space, companions, inventory, landing, main_window, settings, space_editor, statistics, task_editor, task_finished, task_list, task_organizer, task_running, tip_of_the_day, what_now, __init__
+about, attributions, character, choose_constraints, choose_deadline, choose_repeats, choose_skills, choose_space, companions, inventory, landing, main_window, settings, space_editor, statistics, task_checklist, task_editor, task_finished, task_list, task_organizer, task_running, tip_of_the_day, what_now, __init__
 )
