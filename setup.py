2c294761d33007e58c24ee3a96159e394e9d17b1
Anselm Kiefner <watnu@anselm.kiefner.de>
2025-01-28 16:52:59 +0100

    refactor: remove META.stay and update project configuration for Python 3.12

--- setup.py
+++ setup.py
@@ -1,30 +1,3 @@
-import os
-import pathlib
-import sys
+from setuptools import setup
 
-here = os.path.abspath(os.path.dirname(__file__))
-src = os.path.join(here, "src")
-sys.path.append(src)
-
-import use
-from setuptools import find_packages, setup
-
-meta = {}
-load = use(
-    use.URL("https://raw.githubusercontent.com/amogorkon/stay/master/src/stay/stay.py"),
-    modes=use.recklessness,
-).Decoder()
-
-with open("META.stay") as f:
-    for meta in load(f):
-        pass
-
-LONG_DESCRIPTION = pathlib.Path("README.md").read_text()
-setup(
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    zip_safe=False,
-    **meta
-)
+setup()
