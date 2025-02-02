7de54845ca2326888561928bda6a3d8f3b5e15ed
Anselm Kiefner <watnu@anselm.kiefner.de>
2025-02-02 22:24:03 +0100

    refactor: update pyuic6 executable path to Python 3.12 for compatibility

--- qt2py.py
+++ qt2py.py
@@ -6,7 +6,7 @@ ui_path = Path("./src/ui")
 
 for path in ui_path.glob("*.ui"):
     args = [
-        r"C:\Users\micro\AppData\Local\Programs\Python\Python311\Scripts\pyuic6.exe",
+        r"C:\Users\micro\AppData\Local\Programs\Python\Python312\Scripts\pyuic6.exe",
         path,
         "-o",
         f"src/ui/{path.stem}.py",
