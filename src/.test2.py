82254f16d6d782b1b47c00184b96a2e0612d9cd7
Anselm Kiefner <watnu@anselm.kiefner.de>
2025-02-03 05:56:14 +0100

    refactor: remove configuration and test files for cleanup

--- src/.test2.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-query = requests.head("https://github.com/amogorkon/fuzzylogic/blob/master/src/fuzzylogic/functions.py")
-# url_time = query.headers['last-modified']
-# url_date = parsedate(url_time)
-print(query.text)
