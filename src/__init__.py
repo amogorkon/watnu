d2e466affcad724c8e145819b25c7c68d536469d
Anselm Kiefner <watnu@anselm.kiefner.de>
2025-02-05 15:23:11 +0100

    preliminary test stuffs

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ import sqlite3
 import sys
 from pathlib import Path
 from time import time
+from unittest.mock import Mock
 
 import src.configuration as configuration
 from src.app import Application
@@ -11,7 +12,10 @@ app: Application = Application(sys.argv)
 
 home_path = Path.home() / "watnu"
 home_path.mkdir(parents=True, exist_ok=True)
-# sys.args = parser.parse_args() sic!
+
+if not hasattr(sys, "args"):
+    # if watnu.py wasn't the entrypoint, like with tests, sys.args isn't defined, so let's mock
+    sys.args = Mock()
 config_path = home_path / "config_experimental.json" if sys.args.experimental else home_path / "config.json"
 config_path.touch(exist_ok=True)
 config: Config = configuration.read(config_path)
