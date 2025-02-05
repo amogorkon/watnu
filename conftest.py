56ffd85c36574d0136478d673bd53f076e584c4e
Anselm Kiefner <watnu@anselm.kiefner.de>
2025-02-06 00:10:56 +0100

    test: add pytest fixtures for QApplication and Task in conftest.py

---
+++ conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from PyQt6.QtWidgets import QApplication
+from src.classes import Task
+
+
+@pytest.fixture
+def app() -> QApplication:
+    return QApplication([])
+
+
+@pytest.fixture
+def task() -> Task:
+    return Task(id=1, do="Test Task", notes="Test Notes")
