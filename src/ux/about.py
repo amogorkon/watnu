83bed027ac02c4558824725e781091d18931baaa
Anselm Kiefner <watnu@anselm.kiefner.de>
2025-02-15 00:06:03 +0100

    feat: enhance About dialog to display detailed version information

--- src/ux/about.py
+++ src/ux/about.py
@@ -1,5 +1,8 @@
+import sys
+
+import numpy as np
 from PyQt6 import QtWidgets
-from PyQt6.QtCore import QCoreApplication
+from PyQt6.QtCore import PYQT_VERSION_STR, QCoreApplication
 
 import src.ui as ui
 from src.version import __version__
@@ -11,4 +14,10 @@ class About(QtWidgets.QDialog, ui.about.Ui_Dialog):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.version.setText(str(__version__))
+        text = f"""
+Watnu Version: {__version__[0]}.{__version__[1]}.{__version__[2]}
+Python Version: {sys.version_info[0]}.{sys.version_info[1]}.{sys.version_info[2]}
+PyQt Version: {PYQT_VERSION_STR}
+NumPy Version: {np.__version__}
+"""
+        self.version.setText(text)
