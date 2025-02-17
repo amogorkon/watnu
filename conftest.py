1711b6b0bdab4ec27c6569c4d4a90224b9426446
Anselm Kiefner <watnu@anselm.kiefner.de>
2025-02-17 23:22:36 +0100

    feat: add in-memory SQLite database fixture for testing

--- conftest.py
+++ conftest.py
@@ -1,14 +1,24 @@
-import pytest
+import sqlite3
+
 from PyQt6.QtWidgets import QApplication
+from pytest import fixture
 
 from src.classes import Task
 
 
-@pytest.fixture
+@fixture
 def app() -> QApplication:
     return QApplication([])
 
 
-@pytest.fixture
+@fixture
 def task() -> Task:
     return Task(id=1, do="Test Task", notes="Test Notes")
+
+
+@fixture
+def db():
+    conn = sqlite3.connect(":memory:")
+    conn.execute("CREATE TABLE my_table (id INTEGER PRIMARY KEY, value TEXT)")
+    yield conn
+    conn.close()
